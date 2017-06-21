471a27aa6b9e387593c0aa67c98d7dc0cc92b9f7
JasonBilibala <1263489361@qq.com>
2017-06-21 17:17:01 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/jnweather/android/gson/empty.java
@@ -0,0 +1,8 @@
+package com.jnweather.android.gson;
+
+/**
+ * Created by Administrator on 2017/6/21.
+ */
+
+public class empty {
+}
---
+++ app/src/main/java/com/jnweather/android/service/empty.java
@@ -0,0 +1,8 @@
+package com.jnweather.android.service;
+
+/**
+ * Created by Administrator on 2017/6/21.
+ */
+
+public class empty {
+}
