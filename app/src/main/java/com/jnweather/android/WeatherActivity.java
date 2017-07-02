3f4f796f08167c0c104f9c60f50259bc6ae1f3f6
Jason <1263489361@qq.com>
2017-07-02 14:39:00 +0800

    修改程序图标和名称

--- app/src/main/java/com/jnweather/android/WeatherActivity.java
+++ app/src/main/java/com/jnweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.jnweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -11,7 +12,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -21,20 +21,14 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
 import com.jnweather.android.gson.Forecast;
 import com.jnweather.android.gson.Weather;
+import com.jnweather.android.service.AutoUpdateService;
 import com.jnweather.android.util.HttpUtil;
 import com.jnweather.android.util.Utility;
 
-import org.w3c.dom.Text;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -102,6 +96,7 @@ public class WeatherActivity extends AppCompatActivity {
         swipeRefresh = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
         swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navButton = (Button) findViewById(R.id.nav_button);
 
         loadBingPic();
 
@@ -178,6 +173,9 @@ public class WeatherActivity extends AppCompatActivity {
     }
 
     public void showWeatherInfo(Weather weather) {
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
+
         String cityName = weather.basic.cityName;
         String updateTime = weather.basic.update.updateTime.split(" ")[1];
         String degree = weather.now.temperature + "℃";
--- app/src/main/java/com/jnweather/android/service/empty.java
+++
@@ -1,8 +0,0 @@
-package com.jnweather.android.service;
-
-/**
- * Created by Administrator on 2017/6/21.
- */
-
-public class empty {
-}
